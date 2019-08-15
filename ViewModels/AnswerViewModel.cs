2a7fcd4329ad9b261e0f6891e18f5201f4b89fe9
Roberto <reybrujo@gmail.com>
2019-08-15 00:08:00 -0300

    Added support for questions and answers

---
+++ ViewModels/AnswerViewModel.cs
@@ -0,0 +1,34 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+using System.ComponentModel;
+using System.Linq;
+
+namespace TestMakerFree.ViewModels {
+    [JsonObject(MemberSerialization.OptOut)]
+    public class AnswerViewModel {
+        #region Constructor
+        public AnswerViewModel() {
+
+        }
+        #endregion
+
+        #region Properties
+        public int Id { get; set; }
+        public int QuizId { get; set; }
+        public int QuestionId { get; set; }
+        public string Text { get; set; }
+        public string Notes { get; set; }
+        [DefaultValue(0)]
+        public int Type { get; set; }
+        [DefaultValue(0)]
+        public int Flags { get; set; }
+        [DefaultValue(0)]
+        public int Value { get; set; }
+        [JsonIgnore]
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        #endregion
+    }
+}
